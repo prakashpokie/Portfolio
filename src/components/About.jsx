830eb22cebb1f4c3ccbe8a3ae7982c43bf5b6d87
Prakash Gopal <119498896+prakashpokie@users.noreply.github.com>
2024-09-24 13:34:18 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,11 +15,11 @@ const About = () => {
                     <p>Hi. I'm Prakash Gopal, nice to meet you. Please take a look around.</p>
                 </div>
                 <div>
-                    <p>I am passionate about building excellent software that improves
-                    the lives of those around me. I specialize in creating software
-                    for clients ranging from individuals and small-businesses all the
-                    way to large enterprise corporations. What would you do if you had
-                    a software expert available at your fingertips?</p>
+                    <p>I am passionate about harnessing data to drive insightful decisions and improve the lives of those around me. 
+                        I specialize in analyzing complex datasets and building data-driven solutions for clients ranging from individuals 
+                        and small businesses to large enterprise corporations. What would you do if you had a data expert available 
+                        at your fingertips?
+                    </p>
                 </div>
             </div>
         </div>
